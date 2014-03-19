28d009e532806e47b94e204bd878ac69194d682f
Jian Zhou <i@janzhou.org>
2014-03-19 20:25:09 +0800

    crypto.js

--- crypto.js
+++ crypto.js
@@ -2,6 +2,15 @@ var filename = process.argv[2];
 var crypto = require('crypto');
 var fs = require('fs');
 
+//ciphers
+var ciphers = crypto.getCiphers();
+console.log("ciphers: " + ciphers); // ['AES-128-CBC', 'AES-128-CBC-HMAC-SHA1', ...]
+
+//hashes
+var hashes = crypto.getHashes();
+console.log("hashes: " + hashes); // ['sha', 'sha1', 'sha1WithRSAEncryption', ...]
+
+//sha1
 var shasum = crypto.createHash('sha1');
 
 var s = fs.ReadStream(filename);
@@ -11,5 +20,5 @@ s.on('data', function(d) {
 
 s.on('end', function() {
   var d = shasum.digest('hex');
-  console.log(d + '  ' + filename);
+  console.log(filename + ': ' + d);
 });
